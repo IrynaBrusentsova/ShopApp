29d136ae13aac93d492266e8a36a18fa6406cd93
Iryna Brusentsova <bio200480@gmail.com>
2023-03-07 11:19:40 +0200

    Change file content

--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -1,28 +1,36 @@
+
+
+// =====
 import { useState } from 'react';
 import productsData from '../../data/products';
 import Product from '../Product/Product';
+import PropTypes from 'prop-types';
+
 
 const Products = () => {
-  const [products]  = useState(productsData);
+  const [products] = useState(productsData);
 
   return (
     <section>
-      <Product
-        id={products[0].id}
-        name={products[0].name}
-        title={products[0].title}
-        colors={products[0].colors}
-        sizes={products[0].sizes}
-        basePrice={products[0].basePrice} />
-      <Product
-        id={products[1].id}
-        name={products[1].name}
-        title={products[1].title}
-        colors={products[1].colors}
-        sizes={products[1].sizes}
-        basePrice={products[1].basePrice} />
+      {products.map(product => (
+        <Product key={product.name} {...product} />
+      ))}
     </section>
   );
+};
+
+Product.propTypes = {
+  id: PropTypes.number,
+  title: PropTypes.string,
+  name: PropTypes.string,
+  basePrice: PropTypes.number,
+  colors: PropTypes.arrayOf(PropTypes.string),
+  sizes: PropTypes.arrayOf(
+    PropTypes.shape({
+      name: PropTypes.string.isRequired,
+      additionalPrice: PropTypes.number.isRequired,
+    })
+  ),
 };
 
 export default Products;
